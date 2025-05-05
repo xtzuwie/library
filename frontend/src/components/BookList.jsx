f77895f0319f9e3a4a5acf8b4da2045b92a5ede9
xtzuwie <146053979+xtzuwie@users.noreply.github.com>
2025-05-05 20:16:43 +0800

    Update BookList.jsx

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -9,12 +9,16 @@ useEffect(() => {
   fetchbooks();
 }, [fetchbooks])
 
+
+  // Display first 5 books only
+  const limitedBooks = books.slice(0,5);
+  
   return (
     <div className="text-[#252422] bg-white-300 px-4 md:px-12 pb-20">
-      <h1 className="py-6 text-xl md:text-2xl lg:text-3xl w-full mx-auto max-w-6xl">Reader&rsquo;s Favorites</h1>
+      <h1 className="py-6 text-xl md:text-2xl lg:text-3xl w-full mx-auto max-w-6xl">Most Searched Book&rsquo;s</h1>
 
       <div className="flex flex-wrap justify-center gap-5 lg:gap-8 max-w-6xl mx-auto">
-        {books.map((book, index) => (
+        {limitedBooks.map((book, index) => (
           <Link key={index} to={`/book/${book._id}`} className="block">
             <div className="cursor-pointer w-36 md:w-40 xl:w-44 shadow-sm hover:shadow-md rounded-b-md">
               <div className="h-48 md:h-52 xl:h-60 bg-gray-900">
